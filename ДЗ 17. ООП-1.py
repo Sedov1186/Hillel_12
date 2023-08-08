e5b367e8ede188ea58db80f39796872a35dea870
Дмитрий <agent1935@gmail.com>
2023-08-08 18:17:41 +0300

    ad

---
+++ ДЗ 17. ООП-1.py
@@ -0,0 +1,9 @@
+class Avto: #Создать родительский класс auto
+    brand = Tesla #атрибуты
+    age = 1 #атрибуты
+    cоlor = green #атрибуты
+    mark = 2 #атрибуты
+    weight = 3 tons #атрибуты
+
+    def say(self): #методы
+        print()
