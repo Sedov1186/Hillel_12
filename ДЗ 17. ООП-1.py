70ae69a8e008dc4cd3e576cc2f1e54d0a7fa7c45
Дмитрий <agent1935@gmail.com>
2023-08-09 15:47:15 +0300

    add

--- ДЗ 17. ООП-1.py
+++ ДЗ 17. ООП-1.py
@@ -1,9 +1,32 @@
-class Avto: #Создать родительский класс auto
-    brand = Tesla #атрибуты
-    age = 1 #атрибуты
-    cоlor = green #атрибуты
-    mark = 2 #атрибуты
-    weight = 3 tons #атрибуты
+class Auto:
 
-    def say(self): #методы
-        print()
+    def __init__(self, brand, age, mark):
+
+        self.brand = brand
+
+        self.age = age
+
+        self.mark = mark
+
+
+    def move(self):
+
+        print('Выводят сообщение на экран move')
+
+    def stop(self):
+
+        print('Выводят сообщение на экран stop')
+
+    def birthday(self):
+
+        self.age += 1
+
+        print(f'Age {self.age}')
+
+auto_1 = Auto(brand="Tesla", age=1, mark=3)
+
+auto_1.move()
+
+auto_1.stop()
+
+auto_1.birthday()
